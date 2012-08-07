152c7df9c4b5d08876ebaf5d1afb9c857db8608a
presveva <giannidn@gmail.com>
2012-08-07 23:11:09 +0200

    login_required decorator

--- home.py
+++ home.py
@@ -10,6 +10,14 @@ jinja_environment = jinja2.Environment(
   loader=jinja2.FileSystemLoader('templates'))
 jinja_environment.filters['dtf'] = dtf
 
+def login_required(handler_method):
+  def check_login(self):
+    user = users.get_current_user()
+    if not user:
+      return self.redirect(users.create_login_url(self.request.url))
+    else:
+      handler_method(self)
+  return check_login
 
 class BaseHandler(webapp2.RequestHandler):
   def utente(self):
@@ -18,7 +26,6 @@ class BaseHandler(webapp2.RequestHandler):
     return ndb.gql("""SELECT * FROM Tags
         WHERE user = :1 ORDER BY count DESC""", self.utente())
   def generate(self, template_name, template_values={}):
-    user = users.get_current_user()    
     if self.utente():
       bookmarklet = """
 javascript:location.href=
@@ -26,10 +33,10 @@ javascript:location.href=
 '&title='+encodeURIComponent(document.title)+
 '&user='+'%s'+
 '&comment='+document.getSelection().toString()
-""" % (self.request.host_url, user.email())
+""" % (self.request.host_url, self.utente().email())
       url = users.create_logout_url("/")
       linktext = 'Logout'
-      nick = user.email()
+      nick = self.utente().email()
     else:
       bookmarklet = '%s' % self.request.host_url
       url = users.create_login_url(self.request.uri)
@@ -41,7 +48,7 @@ javascript:location.href=
       'nick': nick,
       'url': url,
       'linktext': linktext,
-      'user': user,
+      'user': self.utente(),
       }
     values.update(template_values)
     template = jinja_environment.get_template(template_name)
@@ -62,89 +69,79 @@ class MainPage(BaseHandler):
 
 
 class ArchivedPage(BaseHandler):
+  @login_required
   def get(self):
-    if self.utente():
       bms = ndb.gql("""SELECT * FROM Bookmarks
         WHERE user = :1 AND archived = True 
         ORDER BY data DESC LIMIT 25""", self.utente())
       self.generate('home.html', {'bms'     : bms, 
                                   'tag_list': self.tag_list()
                                   })
-    else:
-      self.generate('hero.html', {})
 
 
 class NotagPage(BaseHandler):
+  @login_required
   def get(self):
-    if self.utente():
       bms = ndb.gql("""SELECT * FROM Bookmarks
         WHERE user = :1 AND have_tags = False
         ORDER BY data DESC""", self.utente())
       self.generate('home.html', {'bms'     : bms, 
                                   'tag_list': self.tag_list()
                                   })
-    else:
-      self.generate('hero.html', {})
 
 
 class PreviewPage(BaseHandler):
+  @login_required
   def get(self):
-    if self.utente():
       bms = ndb.gql("""SELECT * FROM Bookmarks
         WHERE user = :1 AND have_prev = True
         ORDER BY data DESC""", self.utente())
       self.generate('home.html', {'bms'     : bms, 
                                   'tag_list': self.tag_list()
                                   })
-    else:
-      self.generate('hero.html', {})
 
 
 class SearchPage(BaseHandler):
+  @login_required
   def get(self):
-    if users.get_current_user():
-      tag_name = self.request.get('tag')
-      q = ndb.gql("""SELECT * FROM Tags 
-        WHERE user = :1 AND name = :2 
-        ORDER BY data DESC""", self.utente(), tag_name)
-      if q.get().count != 0:
-        self.generate('home.html', {'tag_obj':  q.get(),
-                                    'bms':      q.get().bm_set(),
-                                    'tag_list': q.get().refine_set()
-                                    })
-      else:
-        self.redirect('/')
+    tag_name = self.request.get('tag')
+    q = ndb.gql("""SELECT * FROM Tags 
+      WHERE user = :1 AND name = :2 
+      ORDER BY data DESC""", self.utente(), tag_name)
+    if q.get().count != 0:
+      self.generate('home.html', {'tag_obj':  q.get(),
+                                  'bms':      q.get().bm_set(),
+                                  'tag_list': q.get().refine_set()
+                                  })
     else:
-      self.generate('hero.html', {})
+      self.redirect('/')
 
 
 class RefinePage(BaseHandler):
+  @login_required
   def get(self):
-    if users.get_current_user():
-      tag_name = self.request.get('tag')
-      refine = self.request.get('refine')
-      tag1 = ndb.gql("""SELECT __key__ FROM Tags 
-        WHERE user = :1 AND name = :2""", self.utente(), tag_name).get()
-      tag2 = ndb.gql("""SELECT __key__ FROM Tags 
-        WHERE user = :1 AND name = :2""", self.utente(), refine).get()
-      bms = ndb.gql("""SELECT * FROM Bookmarks 
-        WHERE user = :1 AND tags = :2 AND tags = :3
-        ORDER BY data DESC""", self.utente(), tag1, tag2)
-      self.generate('home.html', {'bms': bms, 'tag_obj': None})
-    else:
-      self.generate('hero.html', {})
+    tag_name = self.request.get('tag')
+    refine = self.request.get('refine')
+    tag1 = ndb.gql("""SELECT __key__ FROM Tags 
+      WHERE user = :1 AND name = :2""", self.utente(), tag_name).get()
+    tag2 = ndb.gql("""SELECT __key__ FROM Tags 
+      WHERE user = :1 AND name = :2""", self.utente(), refine).get()
+    bms = ndb.gql("""SELECT * FROM Bookmarks 
+      WHERE user = :1 AND tags = :2 AND tags = :3
+      ORDER BY data DESC""", self.utente(), tag1, tag2)
+    self.generate('home.html', {'bms': bms, 'tag_obj': None})
 
 
 class EditPage(BaseHandler):
   def get(self):
     bm = Bookmarks.get_by_id(int(self.request.get('bm')))
-    if users.get_current_user() == bm.user:      
+    if self.utente() == bm.user:      
       self.generate('edit.html', {'bm': bm})
     else:
       self.redirect('/')
   def post(self):
     bm = Bookmarks.get_by_id(int(self.request.get('bm')))
-    if users.get_current_user() == bm.user:
+    if self.utente() == bm.user:
       bm.url = self.request.get('url').encode('utf8')
       bm.title = self.request.get('title').encode('utf8')
       bm.comment = self.request.get('comment').encode('utf8')
