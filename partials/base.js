de05d7284d6d90d286c241f66d00bf0f7088dca3
presveva <giannidn@gmail.com>
2012-09-21 19:56:26 +0200

    Responsive design

--- partials/base.js
+++ partials/base.js
@@ -1,52 +1,50 @@
 $(document).ready(function() {
 
-  var tab = $.cookie('active-tab')
-  
+    var tab = $.cookie('active-tab')    
 
-  $('#'+tab).addClass('active');
-  if (tab == 'trash') {
-    $("#dashboard").html('<a>Trashed items</a>');
-    $('#empty_button').removeClass('hide');
-  }
-  if (tab == 'untagged') {
-    $("#dashboard").html('<a>Tagging is important!</a>');
-  }
-  if (tab == 'inbox') {
-    $("#dashboard").html('<a>Welcome to your Inbox</a>');
-  }
-  if (tab == 'starred') {
-    $("#dashboard").html('<a>These are important</a>');
-  }
-  if (tab == 'shared') {
-    $("#dashboard").html('<a>Your shared items</a>');
-  }
-  if (tab == 'archive') {
-    $("#dashboard").html('<a>Your archive</a>');
-  }
-  if (tab == 'friends') {
-    $("#dashboard").html('<a>Your Friends shared items</a>');
-  }
-  if (tab == 'stream') {
-    $("#dashboard").html('<a>Our public stream</a>');
-  }
-  if (tab == 'hero') {
-    $("#dashboard").html('<a>Welcome to {{ brand }}</a>');
-  }
+    $('#'+tab).addClass('active');
+    if (tab == 'trash') {
+        $("#dashboard").html('<a>Trashed items</a>');
+        $('#empty_button').removeClass('hide');
+    }
+    if (tab == 'untagged') {
+        $("#dashboard").html('<a>Tagging is important!</a>');
+    }
+    if (tab == 'inbox') {
+        $("#dashboard").html('<a>Welcome to your Inbox</a>');
+    }
+    if (tab == 'starred') {
+        $("#dashboard").html('<a>These are important</a>');
+    }
+    if (tab == 'shared') {
+        $("#dashboard").html('<a>Your shared items</a>');
+    }
+    if (tab == 'archive') {
+        $("#dashboard").html('<a>Your archive</a>');
+    }
+    if (tab == 'friends') {
+        $("#dashboard").html('<a>Your Friends shared items</a>');
+    }
+    if (tab == 'stream') {
+        $("#dashboard").html('<a>Our public stream</a>');
+    }
+    if (tab == 'hero') {
+        $("#dashboard").html('<a>Welcome to {{ brand }}</a>');
+    }
 
-  $('#addtag').submit(function() {
-    var querystring = $(this).serialize();
-    event.preventDefault();
-    $.ajax({
-      url: '/addtag',
-      data: querystring,
-      success: function() {
-        $("#dashboard").html('<a>New tag created</a>');
-        $('#addtag input').val('');
-        }
-      })
-    })
-
-// $('#navbar').affix({offset: { x: 100 }})
+    $('#addtag').submit(function() {
+        var querystring = $(this).serialize();
+        event.preventDefault();
+        $.ajax({
+            url: '/addtag',
+            data: querystring,
+            success: function() {
+                $("#dashboard").html('<a>New tag created</a>');
+                $('#addtag input').val('');
+                }
+            })
+        })
 
 $('#bookmarklet').tooltip()
+
 });
